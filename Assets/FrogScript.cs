fae08eeb95b39e71b22c206df3c5508452e97ce9
icmor <icornejomora@gmail.com>
2025-02-21 11:08:49 -0600

    Frog animations

--- Assets/FrogScript.cs
+++ Assets/FrogScript.cs
@@ -3,21 +3,35 @@ using UnityEngine;
 public class FrogScript : MonoBehaviour
 {
     public Rigidbody2D body;
+    public Animator animator;
     public float speed = 2f;
     public float jumpSpeed = 4f;
     private bool grounded = true;
     
     void Update()
     {
+        // movement
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = body.velocity.y;
         if (Input.GetKeyDown(KeyCode.Space) && grounded) 
         {
-            body.velocity = new Vector2(Input.GetAxis("Horizontal") * speed, jumpSpeed);
+            vertical = jumpSpeed;
             grounded = false;
         }
-        else
+        body.velocity = new Vector2(horizontal * speed, vertical);
+        
+        // transforms
+        if (horizontal > 0.01f)
         {
-            body.velocity = new Vector2(Input.GetAxis("Horizontal") * speed, body.velocity.y);
+            transform.localScale = new Vector2(1, 1);
         }
+        if (horizontal < -0.01f)
+        {
+            transform.localScale = new Vector2(-1, 1);
+        }
+        
+        // animations
+        animator.SetBool("running", horizontal != 0f);
     }
 
     void OnCollisionEnter2D(Collision2D collision)
@@ -27,12 +41,4 @@ public class FrogScript : MonoBehaviour
             grounded = true;
         }
     }
-
-//    void OnCollisionExit2D(Collision2D collision)
-//    {
-//        if (collision.gameObject.CompareTag("Ground"))
-//        {
-//            grounded = false;
-//        }
-//    }
 }
